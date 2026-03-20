f533a90c8a91527cf68ae2de47cfb0ed5919a255
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-03-20 12:55:47 +0530

    changes done

--- main.tf
+++ main.tf
@@ -139,3 +139,21 @@ resource "aws_route" "database" {
   destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.main.id
 }
+
+resource "aws_route_table_association" "public" {
+    count = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+    count = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "main" {
+    count = length(var.database_subnet_cidrs)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
+}

a6701bdb346c4501ea5dff59574c3450763b8dc5
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-03-20 12:44:24 +0530

    changes done

--- main.tf
+++ main.tf
@@ -127,3 +127,15 @@ resource "aws_nat_gateway" "main" {
   # Without IGW, NAT Gateway won’t work properly
   depends_on = [aws_internet_gateway.igw]
 }
+
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}

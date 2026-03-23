98f889288c4c5900ebde88646be06116faf1d69a
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-03-23 10:08:05 +0530

    changes done

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "vpc_id_output" {
   value = aws_vpc.main.id
 }
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnets" {
+  value = aws_subnet.database[*].id
+}

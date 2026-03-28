9217f159ec2e94014f65420d240aff3264377a7b
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-03-29 02:10:53 +0530

    changes done

--- peering.tf
+++ peering.tf
@@ -24,28 +24,28 @@ resource "aws_vpc_peering_connection" "default" {
   )
 }
 
-resource "aws_route" "public" {
+resource "aws_route" "public_peering" {
     count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
-resource "aws_route" "private" {
+resource "aws_route" "private_peering" {
     count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
-resource "aws_route" "database" {
+resource "aws_route" "database_peering" {
     count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
-resource "aws_route" "deafult" {
+resource "aws_route" "deafult_peering" {
     count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.default.id
   destination_cidr_block    = var.cidr_block
